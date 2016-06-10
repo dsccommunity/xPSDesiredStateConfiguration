67565e911f5d9eb27abf266b6299d92a1d687566
Katie Keim <kakeim@KTK-PC.redmond.corp.microsoft.com>
2016-06-10 10:46:34 -0700

    Adding example.

---
+++ Examples/Sample_xGroupSet.ps1
@@ -0,0 +1,11 @@
+﻿Configuration xGroupSetExample
+{
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration
+                
+    xGroupSet xGroupSet1
+    {
+        GroupName = @('MyGroup1', 'MyGroup2', 'MyGroup3')
+        Ensure = "Present"
+        MembersToInclude = @('Member1', 'Member2')
+    }
+}
