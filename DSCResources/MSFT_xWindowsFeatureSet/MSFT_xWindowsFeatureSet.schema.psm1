0e191dde2e81a4f9cdd2049e8a9fb666ac28a14f
Katie Keim <kakeim@KTK-PC.redmond.corp.microsoft.com>
2016-06-14 10:25:13 -0700

    Adding xWindowsFeatureSet resource.

---
+++ DSCResources/MSFT_xWindowsFeatureSet/MSFT_xWindowsFeatureSet.schema.psm1
@@ -0,0 +1,44 @@
+﻿Import-Module "$PSScriptRoot\..\ResourceSetHelper.psm1"
+
+Configuration xWindowsFeatureSet
+{
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.String[]]
+        $Name,
+
+        [ValidateSet('Present', 'Absent')]
+        [System.String]
+        $Ensure,
+
+        [ValidateNotNullOrEmpty()]
+        [System.String]
+        $Source,
+
+        [System.Boolean]
+        $IncludeAllSubFeature,
+
+        [ValidateNotNull()]
+        [System.Management.Automation.PSCredential]
+        $Credential,
+
+        [ValidateNotNullOrEmpty()]
+        [System.String]
+        $LogPath
+    )
+
+    $commonParameterNames = @("Ensure", "Source", "IncludeAllSubFeature", "Credential", "LogPath")
+    $keyParameterName = "Name"
+    $resourceName = "WindowsFeature"
+
+    # Build common parameters for all xWindowsFeature resource nodes
+    [string] $commonParameters = New-ResourceCommonParameterString -KeyParameterName $keyParamName -CommonParameterNames $commonParameterNames -Parameters $PSBoundParameters
+    
+    # Build xWindowsFeature resource string
+    [string] $resourceString = New-ResourceString -KeyParameterValues $PSBoundParameters[$keyParameterName] -KeyParameterName $keyParameterName -CommonParameters $commonParameters -ResourceName $resourceName
+
+    $configurationScript = [scriptblock]::Create($resourceString)
+    . $configurationScript
+}
---
+++ Tests/Integration/MSFT_xWindowsFeatureSet.ps1
@@ -0,0 +1,114 @@
+﻿function DeployRoleInstallerSourcesFiles 
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [String] 
+        $Path
+    )
+
+	$setupScriptPath = Get-Location
+
+	$testRootPath = (Resolve-Path $setupScriptPath\..\..).Path
+
+	$testSxSPath = Join-Path -Path $testRootPath -ChildPath $Path
+
+	$winSxSPath = Join-Path -Path $env:WinDir -ChildPath 'WinSxS'
+
+	$winSxSReplacePath = $winSxSPath.Replace('\', '\\')
+
+	if (Test-Path $testSxSPath) 
+    {
+        $null = Remove-Item $testSxSPath -Force -Recurse
+    }
+
+	$null = New-Item -Path $testSxSPath -ItemType Directory -Force
+    $telnetFiles = Get-ChildItem $winSxSPath -Filter *telnet* -Recurse
+    
+    foreach ($telnetFile in $telnetFiles) 
+    {
+        $filePath = $telnetFile.FullName
+
+	    if ($filePath -notlike '*server*') 
+        {
+            $destination = $filePath -Replace $winSxSReplacePath, $testSxSPath
+
+	        if (Test-Path $filePath -PathType Leaf)
+            {
+                $null = New-Item -ItemType File -Path $filePath -Force
+            }
+
+	        Copy-Item $filePath $destination -Recurse -Force
+        }
+    }
+
+	return $testSxSPath
+}
+
+$TestEnvironment = Initialize-TestEnvironment `
+    -DSCModuleName 'xPSDesiredStateConfiguration' `
+    -DSCResourceName 'MSFT_xWindowsFeatureSet' `
+    -TestType Integration
+
+Describe "xWindowsFeatureSet Integration Tests" {
+    BeforeAll {
+        Import-Module "$PSScriptRoot\..\Unit\MockServerManager\MockServerManager.psm1"
+        
+        #$script:source = DeployRoleInstallerSourcesFiles -path "TestSxS"
+    }
+
+    AfterAll {}
+
+    It "Install a set of Windows features with their sub features" {
+        $configurationName = "InstallWithSubFeatures"
+        $configurationPath = Join-Path -Path (Get-Location) -ChildPath $configurationName
+        $logPath = Join-Path -Path (Get-Location) -ChildPath "tellnet-client-install"
+        $featureNames = @("Test1", "SubTest3")
+
+        try
+        {
+            Configuration $configurationName
+            {
+                Import-DscResource -ModuleName xPSDesiredStateConfiguration
+
+                xWindowsFeatureSet WindowsFeatureSet1
+                {
+                    Name = $featureNames
+                    Ensure = "Present"
+                    IncludeAllSubFeature = $true
+                    LogPath = $logPath
+                }
+            }
+
+            & $configurationName -OutputPath $configDir
+
+            Start-DscConfiguration -Path $configDir -Wait -Force -Verbose
+
+            # Validate if the Windows features were installed
+            foreach ($featureName in $featureNames)
+            {
+                $windowsFeature = Get-WindowsFeature -Name $featureName
+                $windowsFeature.Installed | Should Be $true
+
+                # Validate sub features were installed correctly
+                foreach ($subfeatureName in $windowsFeature.SubFeatures)
+				{
+					$subfeature = Get-WindowsFeature -Name $subfeatureName
+					$subfeature.Installed | Should Be $true
+				}
+            }    
+        }
+        finally
+        {
+            if (Test-Path -Path $logPath)
+            {
+                Remove-Item -Path $logPath -Recurse -Force
+            }
+
+            if (Test-Path -Path $configurationPath)
+            {
+                Remove-Item -Path $configurationPath -Recurse -Force
+            }
+        }
+    } 
+}
