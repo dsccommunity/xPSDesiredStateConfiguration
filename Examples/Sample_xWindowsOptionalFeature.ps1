7b53b6f0a44244f4fb03d966d2f89b98b75464fb
Daniel Krebs <askdan1el42@gmail.com>
2016-08-16 15:24:41 +0700

    Update MSFT_xWindowsOptionalFeature resource
    1. Fix PSSA issues
    2. Add example for the resource
    3. Add integration test for the resource
    4. Fix lines exceeding 100 characters were it made sense
    5. Rename private functions to follow verb-noun syntax
    6. Update unit test to match renamed functions
    7. Add comment-based help for all functions (public and private)

---
+++ Examples/Sample_xWindowsOptionalFeature.ps1
@@ -0,0 +1,21 @@
+﻿# Installs the Windows optional feature 'TelnetClient'
+
+Configuration Sample_xWindowsOptionalFeature
+{
+    param (
+        [Parameter(Mandatory = $true)]
+        [String]
+        $LogPath
+    )
+
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration
+
+    xWindowsOptionalFeature TelnetClient
+    {
+        Name = 'TelnetClient'
+        Ensure = 'Present'
+        LogPath = $LogPath
+    }
+}
+
+Sample_xWindowsOptionalFeature
