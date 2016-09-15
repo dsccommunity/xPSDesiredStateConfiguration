697761b4400a4766430dc51ccce2b484a5d9cc64
Katie Keim <kakeim@microsoft.com>
2016-09-15 15:56:07 -0700

    Update integration tests to avoid parsing problems

--- Tests/Integration/MSFT_xWindowsOptionalFeature.Integration.Tests.ps1
+++ Tests/Integration/MSFT_xWindowsOptionalFeature.Integration.Tests.ps1
@@ -5,170 +5,148 @@ $script:testEnvironment = Enter-DscResourceTestEnvironment `
     -DscResourceName 'MSFT_xWindowsOptionalFeature' `
     -TestType 'Integration'
 
-Describe 'xWindowsOptionalFeature Integration Tests' {
-    BeforeAll {
-        $script:enabledStates = @( 'Enabled', 'EnablePending' )
-        $script:disabledStates = @( 'Disabled', 'DisablePending' )
-    }
+try
+{
+    Describe 'xWindowsOptionalFeature Integration Tests' {
+        BeforeAll {
+            $script:enabledStates = @( 'Enabled', 'EnablePending' )
+            $script:disabledStates = @( 'Disabled', 'DisablePending' )
+
+            $script:confgurationFilePath = Join-Path -Path $PSScriptRoot -ChildPath 'MSFT_xWindowsOptionalFeature.config.ps1'
+        }
     
-    It 'Should enable a valid Windows optional feature' {
-        $configurationName = 'EnableOptionalFeature'
-        $configurationPath = Join-Path -Path $TestDrive -ChildPath $configurationName
+        It 'Should enable a valid Windows optional feature' {
+            $configurationName = 'EnableWindowsOptionalFeature'
 
-        $logPath = Join-Path -Path $TestDrive -ChildPath 'EnableOptionalFeature.log'
-
-        $validFeatureName = 'TelnetClient'
-
-        $originalFeature = Dism\Get-WindowsOptionalFeature -FeatureName $validFeatureName -Online
-
-        try
-        {
-            if ($originalFeature.State -in $script:enabledStates)
-            {
-                Dism\Disable-WindowsOptionalFeature -FeatureName $validFeatureName -Online -NoRestart
+            $resourceParameters = @{
+                Name = 'TelnetClient'
+                Ensure = 'Present'
+                LogPath = Join-Path -Path $TestDrive -ChildPath 'EnableOptionalFeature.log'
+                NoWindowsUpdateCheck = $true
             }
-        
-            Configuration $configurationName
-            {
-                Import-DscResource -ModuleName 'xPSDesiredStateConfiguration'
 
-                xWindowsOptionalFeature WindowsOptionalFeature1
+            $originalFeature = Dism\Get-WindowsOptionalFeature -FeatureName $resourceParameters.Name -Online
+
+            try
+            {
+                if ($originalFeature.State -in $script:enabledStates)
                 {
-                    Name = $validFeatureName
-                    Ensure = 'Present'
-                    LogPath = $logPath
-                    NoWindowsUpdateCheck = $true
+                    Dism\Disable-WindowsOptionalFeature -FeatureName $resourceParameters.Name -Online -NoRestart
+                }
+
+                { 
+                    . $script:confgurationFilePath -ConfigurationName $configurationName
+                    & $configurationName -OutputPath $TestDrive @resourceParameters
+                    Start-DscConfiguration -Path $TestDrive -ErrorAction 'Stop' -Wait -Force
+                } | Should Not Throw
+
+                $windowsOptionalFeature = Dism\Get-WindowsOptionalFeature -FeatureName $resourceParameters.Name -Online
+
+                $windowsOptionalFeature | Should Not Be $null
+                $windowsOptionalFeature.State -in $script:enabledStates | Should Be $true
+            }
+            finally
+            {
+                if ($originalFeature.State -in $script:disabledStates)
+                {
+                    Dism\Disable-WindowsOptionalFeature -FeatureName $resourceParameters.Name -Online -NoRestart
+                }
+                elseif ($originalFeature.State -in $script:enabledStates)
+                {
+                    Dism\Enable-WindowsOptionalFeature -FeatureName $resourceParameters.Name -Online -NoRestart
+                }
+
+                if (Test-Path -Path $resourceParameters.LogPath)
+                {
+                    Remove-Item -Path $resourceParameters.LogPath -Recurse -Force
                 }
             }
-
-            { & $configurationName -OutputPath $configurationPath } | Should Not Throw
-
-            { Start-DscConfiguration -Path $configurationPath -Wait -Force } | Should Not Throw
-
-            $windowsOptionalFeature = Dism\Get-WindowsOptionalFeature -FeatureName $validFeatureName -Online
-
-            $windowsOptionalFeature | Should Not Be $null
-            $windowsOptionalFeature.State -in $script:enabledStates| Should Be $true
         }
-        finally
-        {
-            if ($originalFeature.State -in $script:disabledStates)
-            {
-                Dism\Disable-WindowsOptionalFeature -FeatureName $validFeatureName -Online -NoRestart
+
+        It 'Should disable a valid Windows optional feature' {
+            $configurationName = 'DisableWindowsOptionalFeature'
+
+            $resourceParameters = @{
+                Name = 'TelnetClient'
+                Ensure = 'Absent'
+                LogPath = Join-Path -Path $TestDrive -ChildPath 'DisableOptionalFeature.log'
+                NoWindowsUpdateCheck = $true
+                RemoveFilesOnDisable = $false
             }
 
-            if (Test-Path -Path $logPath) {
-                Remove-Item -Path $logPath -Recurse -Force
+            $originalFeature = Dism\Get-WindowsOptionalFeature -FeatureName $resourceParameters.Name -Online
+
+            try
+            {
+                if ($originalFeature.State -in $script:disabledStates)
+                {
+                    Dism\Enable-WindowsOptionalFeature -FeatureName $resourceParameters.Name -Online -NoRestart
+                }
+
+                { 
+                    . $script:confgurationFilePath -ConfigurationName $configurationName
+                    & $configurationName -OutputPath $TestDrive @resourceParameters
+                    Start-DscConfiguration -Path $TestDrive -ErrorAction 'Stop' -Wait -Force
+                } | Should Not Throw
+
+                $windowsOptionalFeature = Dism\Get-WindowsOptionalFeature -FeatureName $resourceParameters.Name -Online
+
+                $windowsOptionalFeature | Should Not Be $null
+                $windowsOptionalFeature.State -in $script:disabledStates | Should Be $true
+            }
+            finally
+            {
+                if ($originalFeature.State -in $script:disabledStates)
+                {
+                    Dism\Disable-WindowsOptionalFeature -FeatureName $resourceParameters.Name -Online -NoRestart
+                }
+                elseif ($originalFeature.State -in $script:enabledStates)
+                {
+                    Dism\Enable-WindowsOptionalFeature -FeatureName $resourceParameters.Name -Online -NoRestart
+                }
+
+                if (Test-Path -Path $resourceParameters.LogPath)
+                {
+                    Remove-Item -Path $resourceParameters.LogPath -Recurse -Force
+                }
+            }
+        }
+
+        It 'Should not enable an incorrect Windows optional feature' {
+            $configurationName = 'EnableIncorrectWindowsOptionalFeature'
+
+            $resourceParameters = @{
+                Name = 'NonExistentWindowsOptionalFeature'
+                Ensure = 'Present'
+                LogPath = Join-Path -Path $TestDrive -ChildPath 'EnableIncorrectWindowsFeature.log'
+                NoWindowsUpdateCheck = $true
             }
 
-            if (Test-Path -Path $configurationPath)
+            Dism\Get-WindowsOptionalFeature -FeatureName $resourceParameters.Name -Online | Should Be $null
+
+            try
             {
-                Remove-Item -Path $configurationPath -Recurse -Force
+                { 
+                    . $script:confgurationFilePath -ConfigurationName $configurationName
+                    & $configurationName -OutputPath $TestDrive @resourceParameters
+                    Start-DscConfiguration -Path $TestDrive -ErrorAction 'Stop' -Wait -Force
+                } | Should Throw "Feature name $($resourceParameters.Name) is unknown."
+
+                Test-Path -Path $resourceParameters.LogPath | Should Be $true
+
+                Dism\Get-WindowsOptionalFeature -FeatureName $resourceParameters.Name -Online | Should Be $null
+            }
+            finally
+            {
+                if (Test-Path -Path $resourceParameters.LogPath)
+                {
+                    Remove-Item -Path $resourceParameters.LogPath -Recurse -Force
+                }
             }
         }
     }
-
-    It 'Should disable a valid Windows optional feature' {
-        $configurationName = 'DisableOptionalFeature'
-        $configurationPath = Join-Path -Path $TestDrive -ChildPath $configurationName
-
-        $logPath = Join-Path -Path $TestDrive -ChildPath 'DisableOptionalFeature.log'
-
-        $validFeatureName = 'TelnetClient'
-
-        $originalFeature = Dism\Get-WindowsOptionalFeature -FeatureName $validFeatureName -Online
-
-        try
-        {
-            if ($originalFeature.State -in $script:disabledStates)
-            {
-                Dism\Enable-WindowsOptionalFeature -FeatureName $validFeatureName -Online -NoRestart
-            }
-
-            Configuration $configurationName
-            {
-                Import-DscResource -ModuleName 'xPSDesiredStateConfiguration'
-
-                xWindowsOptionalFeature WindowsOptionalFeature1
-                {
-                    Name = $validFeatureName
-                    Ensure = 'Absent'
-                    LogPath = $logPath
-                    NoWindowsUpdateCheck = $true
-                }
-            }
-
-            { & $configurationName -OutputPath $configurationPath } | Should Not Throw
-
-            { Start-DscConfiguration -Path $configurationPath -Wait -Force } | Should Not Throw
-
-            $windowsOptionalFeature = Dism\Get-WindowsOptionalFeature -FeatureName $validFeatureName -Online 
-
-            $windowsOptionalFeature | Should Not Be $null
-            $windowsOptionalFeature.State -in $script:disabledStates | Should Be $true
-        }
-        finally
-        {
-            if ($originalFeature.State -in $script:disabledStates)
-            {
-                Dism\Disable-WindowsOptionalFeature -FeatureName $validFeatureName -Online -NoRestart
-            }
-
-            if (Test-Path -Path $logPath) {
-                Remove-Item -Path $logPath -Recurse -Force
-            }
-
-            if (Test-Path -Path $configurationPath)
-            {
-                Remove-Item -Path $configurationPath -Recurse -Force
-            }
-        }
-    }
-
-    It 'Should not enable an incorrect Windows optional feature' {
-        $configurationName = 'EnableIncorrectWindowsFeature'
-        $configurationPath = Join-Path -Path $TestDrive -ChildPath $configurationName
-
-        $logPath = Join-Path -Path $TestDrive -ChildPath 'EnableIncorrectWindowsFeature.log'
-
-        $invalidFeatureName = 'NonExistentWindowsOptionalFeature'
-
-        Dism\Get-WindowsOptionalFeature -FeatureName $invalidFeatureName -Online | Should Be $null
-
-        try
-        {
-            Configuration $configurationName
-            {
-                Import-DscResource -ModuleName 'xPSDesiredStateConfiguration'
-
-                xWindowsOptionalFeature WindowsOptionalFeature1
-                {
-                    Name = $invalidFeatureName
-                    Ensure = 'Present'
-                    LogPath = $logPath
-                }
-            }
-
-            { & $configurationName -OutputPath $configurationPath } | Should Not Throw
-
-            { Start-DscConfiguration -Path $configurationPath -ErrorAction 'Stop' -Wait -Force } |
-                Should Throw "Feature name $invalidFeatureName is unknown."
-
-            Test-Path -Path $logPath | Should Be $true
-
-            Dism\Get-WindowsOptionalFeature -FeatureName $invalidFeatureName -Online | Should Be $null
-        }
-        finally
-        {
-            if (Test-Path -Path $logPath)
-            {
-                Remove-Item -Path $logPath -Recurse -Force
-            }
-
-            if (Test-Path -Path $configurationPath)
-            {
-                Remove-Item -Path $configurationPath -Recurse -Force
-            }
-        }
-    }
+}
+finally
+{
+    Exit-DscResourceTestEnvironment -TestEnvironment $script:testEnvironment
 }
