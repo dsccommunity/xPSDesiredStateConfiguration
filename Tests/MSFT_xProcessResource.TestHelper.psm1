df88360ca96fec9ad057164e255f2652c9cbd125
Mariah Breakey <mbreakey@microsoft.com>
2016-12-08 17:17:04 -0800

    adding helper back in since ProcessSet uses it

---
+++ Tests/MSFT_xProcessResource.TestHelper.psm1
@@ -0,0 +1,28 @@
+﻿
+Import-Module -Name (Join-Path -Path (Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) `
+                                                -ChildPath 'DSCResources') `
+                               -ChildPath 'CommonResourceHelper.psm1')
+
+<#
+    .SYNOPSIS
+        Stops all instances of the process with the given name.
+
+    .PARAMETER ProcessName
+        The name of the process to stop.
+#>
+function Stop-ProcessByName
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [String]
+        $ProcessName
+    )
+
+    Stop-Process -Name $ProcessName -ErrorAction 'SilentlyContinue' -Force
+    Wait-ScriptBlockReturnTrue -ScriptBlock { return $null -eq (Get-Process -Name $ProcessName -ErrorAction 'SilentlyContinue') } `
+                               -TimeoutSeconds 15
+}
+
+Export-ModuleMember -Function Stop-ProcessByName
