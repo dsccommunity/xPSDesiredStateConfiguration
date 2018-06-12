2b03573346837d9160487d96136c69be6bb94f38
Johan Ljunggren <johan.ljunggren@shamn.se>
2018-06-12 16:12:35 +0200

    Update examples to be able to be published

--- Examples/xScript_WatchFileContentConfig.ps1
+++ Examples/xScript_WatchFileContentConfig.ps1
@@ -1,3 +1,23 @@
+
+<#PSScriptInfo
+.VERSION 1.0.0
+.GUID f9306ebe-8af5-4dee-baf3-f3fac17891db
+.AUTHOR Microsoft Corporation
+.COMPANYNAME Microsoft Corporation
+.COPYRIGHT
+.TAGS DSCConfiguration
+.LICENSEURI https://github.com/PowerShell/xPSDesiredStateConfiguration/blob/master/LICENSE
+.PROJECTURI https://github.com/PowerShell/xPSDesiredStateConfiguration
+.ICONURI
+.EXTERNALMODULEDEPENDENCIES xPSDesiredStateConfiguration
+.REQUIREDSCRIPTS
+.EXTERNALSCRIPTDEPENDENCIES
+.RELEASENOTES First version.
+.PRIVATEDATA 2016-Datacenter,2016-Datacenter-Server-Core
+#>
+
+#Requires -module @{ModuleName = 'xPSDesiredStateConfiguration';ModuleVersion = '8.2.0.0'}
+
 <#
     .SYNOPSIS
         Creates a file at the given file path with the specified content through
@@ -14,7 +34,7 @@
         The content to set for the new file.
         Defaults to 'Just some sample text to write to the file'.
 #>
-Configuration Sample_xScript {
+Configuration xScript_WatchFileContentConfig {
     [CmdletBinding()]
     param
     (
@@ -29,7 +49,7 @@ Configuration Sample_xScript {
         $FileContent
     )
 
-    Import-DscResource -ModuleName 'xPSDesiredStateConfiguration'
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration
 
     Node localhost
     {
@@ -62,9 +82,10 @@ Configuration Sample_xScript {
                 }
 
                 return @{
-                    Result = Get-Content -Path $fileContent
+                    Result = $fileContent
                 }
             }
         }
     }
 }
+
