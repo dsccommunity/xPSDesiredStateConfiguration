a2e729778ae8575da1935fa784ecef3e04ce2ea2
Mike Hendrickson <mhendric@microsoft.com>
2019-02-13 14:40:25 -0800

    Add space between comment hashtags and comments - Post Review #1

--- Examples/Sample_xDscWebServiceRegistration_UseSQLProvider.ps1
+++ Examples/Sample_xDscWebServiceRegistration_UseSQLProvider.ps1
@@ -1,84 +1,97 @@
-# DSC configuration for Pull Server using registration with enhanced security settings
-
-# The Sample_xDscWebServiceRegistration_UseSQLProvider configuration sets up a DSC pull server that is capable for client nodes
-# to register with it and use SQL Server as a backend DB
-
-# Prerequisite:1- Install a certificate in "CERT:\LocalMachine\MY\" store
-#               For testing environments, you could use a self-signed certificate. (New-SelfSignedCertificate cmdlet could generate one for you).
-#               For production environments, you will need a certificate signed by valid CA.
-#               Registration only works over https protocols. So to use registration feature, a secure pull server setup with certificate is necessary
-#              2- Install and Configure SQL Server
-
-# The Sample_MetaConfigurationToRegisterWithSecurePullServer register a DSC client node with the pull server
-
-# ======================================== Arguments ======================================== #
-
 <#
-    Check if OS major version is higher or equal to 10.
+    .SYNOPSIS
+        The Sample_xDscWebServiceRegistration_UseSQLProvider configuration sets
+        up a DSC pull server that is capable for client nodes to register with
+        it and use SQL Server as a backend DB.
 
-    Note: This check is to pass example validation CI tests,
-          it has not been tested to run on Windows Server 2012 R2,
-          please see the following example for a Windows Server 2012 R2 version
-          of this example;
-          https://github.com/PowerShell/xPSDesiredStateConfiguration/blob/master/Examples/Sample_xDscWebServiceRegistration_Win2k12and2k12R2.ps1.
+        Prerequisite: 1 - Install a certificate in 'CERT:\LocalMachine\MY\'
+                          store. For testing environments, you could use a
+                          self-signed certificate. (New-SelfSignedCertificate
+                          cmdlet could generate one for you). For production
+                          environments, you will need a certificate signed by
+                          valid CA. Registration only works over https
+                          protocols. So to use registration feature, a secure
+                          pull server setup with certificate is necessary.
+                      2 - Install and Configure SQL Server
+
+    .PARAMETER NodeName
+        The name of the node being configured as a DSC Pull Server.
+
+    .PARAMETER CertificateThumbPrint
+        Certificate thumbprint for creating an HTTPS endpoint. Use
+        "AllowUnencryptedTraffic" for setting up a non SSL based endpoint.
+
+    .PARAMETER RegistrationKey
+        This key will be used by client nodes as a shared key to authenticate
+        during registration. This should be a string with enough entropy
+        (randomness) to protect the registration of clients to the pull server.
+        The example creates a new GUID for the registration key.
+
+    .EXAMPLE
+        Check if OS major version is higher or equal to 10.
+
+        Note: This check is to pass example validation CI tests,
+              it has not been tested to run on Windows Server 2012 R2,
+              please see the following example for a Windows Server 2012 R2 version
+              of this example;
+              https://github.com/PowerShell/xPSDesiredStateConfiguration/blob/master/Examples/Sample_xDscWebServiceRegistration_Win2k12and2k12R2.ps1.
+
+        if ([Environment]::OSVersion.Version.Major -ge '10')
+        {
+            $thumbprint = (New-SelfSignedCertificate -Subject $env:COMPUTERNAME).Thumbprint
+        }
+        else
+        {
+            Write-Warning -Message 'Running on operating system older than major version 10, this configuration is not meant to run on OS with a major version older than version 10. Generating certificate using New-SelfSignedCertificate with an alternate method.'
+            $thumbprint = (New-SelfSignedCertificate -DnsName $env:COMPUTERNAME -CertStoreLocation cert:\LocalMachine\My ).Thumbprint
+        }
+
+        $registrationKey = [System.Guid]::NewGuid()
+
+        Sample_xDscWebServiceRegistration_UseSQLProvider -RegistrationKey $registrationKey -CertificateThumbPrint $thumbprint -Verbose
 #>
-if ([Environment]::OSVersion.Version.Major -ge '10')
-{
-    $thumbprint = (New-SelfSignedCertificate -Subject $env:COMPUTERNAME).Thumbprint
-}
-else
-{
-    Write-Warning -Message 'Running on operating system older than major version 10, this configuration is not meant to run on OS with a major version older than version 10. Generating certificate using New-SelfSignedCertificate with an alternate method.'
-    $thumbprint = (New-SelfSignedCertificate -DnsName $env:COMPUTERNAME -CertStoreLocation cert:\LocalMachine\My ).Thumbprint
-}
-
-$registrationKey = [guid]::NewGuid()
-
-# ======================================== Arguments ======================================== #
-
-# =================================== Section Pull Server =================================== #
-
-configuration Sample_xDscWebServiceRegistration_UseSQLProvider
+Configuration Sample_xDscWebServiceRegistration_UseSQLProvider
 {
     param
     (
-        [string[]]
+        [Parameter()]
+        [System.String[]]
         $NodeName = 'localhost',
 
+        [Parameter()]
         [ValidateNotNullOrEmpty()]
-        [string]
+        [System.String]
         $CertificateThumbPrint,
 
-        [Parameter(HelpMessage = 'This should be a string with enough entropy (randomness) to protect the registration of clients to the pull server.  We will use new GUID by default.')]
+        [Parameter()]
         [ValidateNotNullOrEmpty()]
-        [string]
-        # A guid that clients use to initiate conversation with pull server
+        [System.String]
         $RegistrationKey
     )
 
     Import-DSCResource -ModuleName xPSDesiredStateConfiguration
-    # To explicitly import the resource WindowsFesture and File.
+    # To explicitly import the resource WindowsFeature and File.
     Import-DscResource -ModuleName PSDesiredStateConfiguration
 
     Node $NodeName
     {
         WindowsFeature DSCServiceFeature
         {
-            Ensure = "Present"
-            Name   = "DSC-Service"
+            Ensure = 'Present'
+            Name   = 'DSC-Service'
         }
 
         xDscWebService PSDSCPullServer
         {
-            Ensure                       = "Present"
-            EndpointName                 = "PSDSCPullServer"
+            Ensure                       = 'Present'
+            EndpointName                 = 'PSDSCPullServer'
             Port                         = 8080
             PhysicalPath                 = "$env:SystemDrive\inetpub\PSDSCPullServer"
-            CertificateThumbPrint        = $certificateThumbPrint
+            CertificateThumbPrint        = $CertificateThumbPrint
             ModulePath                   = "$env:PROGRAMFILES\WindowsPowerShell\DscService\Modules"
             ConfigurationPath            = "$env:PROGRAMFILES\WindowsPowerShell\DscService\Configuration"
-            State                        = "Started"
-            DependsOn                    = "[WindowsFeature]DSCServiceFeature"
+            State                        = 'Started'
+            DependsOn                    = '[WindowsFeature]DSCServiceFeature'
             RegistrationKeyPath          = "$env:PROGRAMFILES\WindowsPowerShell\DscService"
             AcceptSelfSignedCertificates = $true
             UseSecurityBestPractices     = $true
@@ -96,28 +109,47 @@ configuration Sample_xDscWebServiceRegistration_UseSQLProvider
     }
 }
 
-Sample_xDscWebServiceRegistration_UseSQLProvider -RegistrationKey $registrationKey -CertificateThumbPrint $thumbprint -Verbose
-# =================================== Section Pull Server =================================== #
+<#
+    .SYNOPSIS
+        The Sample_MetaConfigurationToRegisterWithSecurePullServer registers
+        a DSC client node with the pull server.
 
-# =================================== Section DSC Client =================================== #
+    .PARAMETER NodeName
+        The name of the node being configured as a DSC Pull Server.
 
+    .PARAMETER RegistrationKey
+        This key will be used by client nodes as a shared key to authenticate
+        during registration. This should be a string with enough entropy
+        (randomness) to protect the registration of clients to the pull server.
+        The example creates a new GUID for the registration key.
+
+    .PARAMETER ServerName
+        The HostName to use when configuring the Pull Server URL on the DSC
+        client.
+
+    .EXAMPLE
+        $registrationKey = [System.Guid]::NewGuid()
+
+        Sample_MetaConfigurationToRegisterWithSecurePullServer -RegistrationKey $registrationKey
+#>
 [DSCLocalConfigurationManager()]
-configuration Sample_MetaConfigurationToRegisterWithSecurePullServer
+Configuration Sample_MetaConfigurationToRegisterWithSecurePullServer
 {
     param
     (
+        [Parameter()]
         [ValidateNotNullOrEmpty()]
-        [string]
+        [System.String]
         $NodeName = 'localhost',
 
+        [Parameter()]
         [ValidateNotNullOrEmpty()]
-        [string]
-        # Same as the one used to setup pull server in previous configuration
+        [System.String]
         $RegistrationKey,
 
+        [Parameter()]
         [ValidateNotNullOrEmpty()]
-        [string]
-        # Node name of the pull server, same as $NodeName used in previous configuration
+        [System.String]
         $ServerName = 'localhost'
     )
 
@@ -130,19 +162,15 @@ configuration Sample_MetaConfigurationToRegisterWithSecurePullServer
 
         ConfigurationRepositoryWeb CONTOSO-PullSrv
         {
-            ServerURL          = "https://$ServerName`:8080/PSDSCPullServer.svc" # notice it is https
+            ServerURL          = "https://$ServerName`:8080/PSDSCPullServer.svc"
             RegistrationKey    = $RegistrationKey
             ConfigurationNames = @('ClientConfig')
         }
 
         ReportServerWeb CONTOSO-PullSrv
         {
-            ServerURL       = "https://$ServerName`:8080/PSDSCPullServer.svc" # notice it is https
+            ServerURL       = "https://$ServerName`:8080/PSDSCPullServer.svc"
             RegistrationKey = $RegistrationKey
         }
     }
 }
-
-Sample_MetaConfigurationToRegisterWithSecurePullServer -RegistrationKey $registrationKey
-
-# =================================== Section DSC Client =================================== #
