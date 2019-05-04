b794ec5de9a4f1269cfdbee29e60104cf8afab04
Thomas Meckel <tmeckel@noreply.github.com>
2019-05-04 13:26:06 +0200

    Added additional integration test DSC configuration to MSFT_xDSCWebService

--- Tests/Integration/MSFT_xDSCWebService.config.ps1
+++ Tests/Integration/MSFT_xDSCWebService.config.ps1
@@ -142,3 +142,90 @@ Configuration MSFT_xDSCWebService_PullTestWithoutFirewall_Config
         }
     }
 }
+
+<#
+    .SYNOPSIS
+        Sets up a DSC pull server without firewall exceptions
+#>
+Configuration MSFT_xDSCWebService_PullTestWithExternalFirewall_Config
+{
+    Import-DscResource -ModuleName 'xPSDesiredStateConfiguration'
+    Import-DscResource -ModuleName 'NetworkingDsc'
+
+    node $AllNodes.NodeName
+    {
+
+        xDSCWebService Integration_Test
+        {
+            Ensure                       = 'Present'
+            AcceptSelfSignedCertificates = $true
+            CertificateThumbPrint        = $Node.CertificateThumbprint
+            ConfigurationPath            = $Node.ConfigurationPath
+            Enable32BitAppOnWin64        = $false
+            EndpointName                 = $Node.EndpointName
+            ModulePath                   = $Node.ModulePath
+            Port                         = $Node.Port
+            PhysicalPath                 = $Node.PhysicalPath
+            RegistrationKeyPath          = $Node.RegistrationKeyPath
+            State                        = 'Started'
+            UseSecurityBestPractices     = $true
+            ConfigureFirewall            = $false
+            DependsOn                    = '[Firewall]DSC-FirewallRule'
+        }
+
+        Firewall PSDSCPullServerRule
+        {
+            Ensure      = 'Present'
+            Name        = "DSC_PullServer_$Port"
+            DisplayName = "DSC PullServer $Port"
+            Group       = 'DSC PullServer'
+            Enabled     = $true
+            Action      = 'Allow'
+            Direction   = 'InBound'
+            LocalPort   = $Node.Port
+            Protocol    = 'TCP'
+            DependsOn   = '[xDscWebService]Integration_Test'
+        }
+
+    }
+}
+
+<#
+    .SYNOPSIS
+        Sets up a DSC pull server with an separately defined application pool
+#>
+Configuration MSFT_xDSCWebService_PullTestExternalAppPool_Config
+{
+    Import-DscResource -ModuleName 'xPSDesiredStateConfiguration'
+    Import-DscResource -ModuleName 'xWebAdministration'
+
+    node $AllNodes.NodeName
+    {
+
+        xWebAppPool PSDSCPullServerPool
+        {
+            Ensure       = 'Present'
+            Name         = "PSDSCPullServer_$($Node.EndpointName)"
+            IdentityType = 'NetworkService'
+        }
+
+        xDSCWebService Integration_Test
+        {
+            Ensure                       = 'Present'
+            AcceptSelfSignedCertificates = $true
+            CertificateThumbPrint        = $Node.CertificateThumbprint
+            ConfigurationPath            = $Node.ConfigurationPath
+            Enable32BitAppOnWin64        = $false
+            EndpointName                 = $Node.EndpointName
+            ModulePath                   = $Node.ModulePath
+            Port                         = $Node.Port
+            PhysicalPath                 = $Node.PhysicalPath
+            RegistrationKeyPath          = $Node.RegistrationKeyPath
+            State                        = 'Started'
+            UseSecurityBestPractices     = $true
+            ConfigureFirewall            = $false
+            DependsOn                    = '[xWebAppPool]PSDSCPullServerPool'
+        }
+
+    }
+}
